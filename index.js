a524764a7cccd7362e07a3d12388c49b623e378c
Outkarm <108669691+Outkarm@users.noreply.github.com>
2022-10-25 13:23:41 +0000

    fix linter errors

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // BookListApp class: class to hold books
-import { appendBook, displayBooks } from "./module/addBook.js";
-import { getDate } from "./module/time-and-date.js";
-import { navLinks } from "./module/navbar-switch.js";
-import { removeButton } from "./module/total-delete-book.js";
+import { appendBook, displayBooks } from './module/addBook.js';
+import { getDate } from './module/time-and-date.js';
+import { navLinks } from './module/navbar-switch.js';
+import { removeButton } from './module/total-delete-book.js';
 
 // instantiate class
 
@@ -13,4 +13,4 @@ removeButton();
 navLinks();
 getDate();
 
-document.addEventListener("DOMContentLoaded", displayBooks());
+document.addEventListener('DOMContentLoaded', displayBooks());
--- module/addBook.js
+++ module/addBook.js
@@ -1,8 +1,8 @@
-import { clearFields } from "./clear-field.js";
+import { clearFields } from './clear-field.js';
 
 const addBookToList = (book) => {
-  const container = document.querySelector(".book-container");
-  const row = document.createElement("tr");
+  const container = document.querySelector('.book-container');
+  const row = document.createElement('tr');
   row.innerHTML = `
       <td>"${book.title}" by ${book.author}</td>
       <td><button class= "remove-btn" id=${book.Id}>Remove</button></td>
@@ -12,21 +12,21 @@ const addBookToList = (book) => {
 
 const appendBook = () => {
   const bookList = [];
-  const form = document.querySelector("form");
-  form.addEventListener("submit", (e) => {
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const title = document.querySelector(".title").value;
-    const author = document.querySelector(".author").value;
+    const title = document.querySelector('.title').value;
+    const author = document.querySelector('.author').value;
     const Id = bookList.length;
     addBookToList({ title, author, Id });
     bookList.push({ title, author, Id });
-    localStorage.setItem("books", JSON.stringify(bookList));
+    localStorage.setItem('books', JSON.stringify(bookList));
     clearFields();
   });
 };
 
 const displayBooks = () => {
-  const books = JSON.parse(localStorage.getItem("books"));
+  const books = JSON.parse(localStorage.getItem('books'));
   books.forEach((book) => addBookToList(book));
 };
 
